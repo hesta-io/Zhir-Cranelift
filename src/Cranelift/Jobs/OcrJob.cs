52902f858eb0470fa3537abceb379d1bf1222892
Muhammad Azeez <muhammad-azeez@outlook.com>
2020-12-19 12:02:21 +0300

    use ckb+eng in cranelift

--- src/Cranelift/Jobs/OcrJob.cs
+++ src/Cranelift/Jobs/OcrJob.cs
@@ -292,7 +292,7 @@ namespace Cranelift.Jobs
 
             if (page.Succeeded)
             {
-                var result = await RunTesseract(donePath, cancellationToken, "ckb");
+                var result = await RunTesseract(donePath, cancellationToken, "ckb+eng");
                 page.Succeeded = result.Success;
 
                 if (page.Succeeded)
